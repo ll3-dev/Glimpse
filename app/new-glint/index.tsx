8d572315152aa4a9f4d39b34e43a26782010a5f6
loopy-lim <loopy-lim@users.noreply.github.com>
2026-01-31 14:09:32 +0900

    refactor: new-glint 페이지를 디렉토리 구조로 리팩토링 파일 기반 라우팅 구조 개선을 위해 new-glint.tsx를 new-glint/index.tsx 디렉토리 구조로 변경합니다.

--- app/new-glint/index.tsx
+++ app/new-glint/index.tsx
@@ -1,12 +1,11 @@
 import AddGlint from "@/components/glint/add";
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function NewGlintScreen() {
   return (
     <>
       <Stack.Screen
-        name="new-glint"
         options={{
           title: "새로운 Glint",
         }}
